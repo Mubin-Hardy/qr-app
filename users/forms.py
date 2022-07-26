842f6f3e3eee4cf5385545f0036a1633d62363e4
Mubin-Hardy <mubinmattel@gmail.com>
2022-07-26 14:59:24 +0530

    kjhu

---
+++ users/forms.py
@@ -0,0 +1,94 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import Profile
+
+
+class RegisterForm(UserCreationForm):
+    # fields we want to include and customize in our form
+    first_name = forms.CharField(max_length=100,
+                                 required=True,
+                                 widget=forms.TextInput(attrs={'placeholder': 'First Name',
+                                                               'class': 'form-control',
+                                                               }))
+    last_name = forms.CharField(max_length=100,
+                                required=True,
+                                widget=forms.TextInput(attrs={'placeholder': 'Address',
+                                                              'class': 'form-control',
+                                                              }))
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+
+    adhaar_no = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Adhaar_No',
+                                                             'class': 'form-control',
+                                                             }))
+    pan_no = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'PAN_No',
+                                                             'class': 'form-control',
+                                                             }))
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'adhaar_no','pan_no']
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+    password = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                 'class': 'form-control',
+                                                                 'data-toggle': 'password',
+                                                                 'id': 'password',
+                                                                 'name': 'password',
+                                                                 }))
+    remember_me = forms.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password','file', 'remember_me']
+
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
---
+++ users/views.py
@@ -0,0 +1,149 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from .models import QrCode
+from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm,forms
+from django.conf import settings
+from qrcode import *
+import time
+from qr_code.qrcode.utils import QRCodeOptions
+import qrcode
+from .forms import NewUserForm
+from django.contrib.auth import login
+def signup(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+			messages.success(request, "Registration successful." )
+			return render(request,"users/index2.html")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="users/signup.html", context={"register_form":form})
+
+def home(request):
+    return render(request, 'users/home.html')
+def qr_gen(request):
+    if request.method == 'POST':
+        data = request.POST['data']
+        img = qrcode.make(data)
+        img_name = 'qr' + str(time.time()) + '.png'
+        img.save(settings.MEDIA_ROOT + '/' + img_name)
+        return render(request, 'users/index.html', {'img_name': img_name})
+    return render(request, 'users/index.html')
+
+class RegisterView(View):
+    form_class = RegisterForm
+    initial = {'key': 'value'}
+    template_name = 'users/register.html'
+
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        print(form)
+        print("okk")
+        x = True
+
+        if x:
+            print("hiiii")
+
+
+            username = form.cleaned_data.get('username')
+            adhaar = form.cleaned_data.get('adhaar_no')
+            pan = form.cleaned_data.get('pan_no')
+            address = form.cleaned_data.get('last_name')
+            firstname = form.cleaned_data.get('first_name')
+            full ="Name" + "\n" + str(firstname) + "\n"+ "Address" + "\n" + str(address) + "\n"+ "Adhar number" + "\n" + str(adhaar) + "\n" +"Pan number" + "\n" + str(pan)
+            img = qrcode.make(full)
+            img_name = 'qr' + str(time.time()) + '.png'
+            img.save(settings.MEDIA_ROOT + '/' + img_name) 
+            messages.success(request, f'Account created for {username} {adhaar}')
+            return render(request, 'users/index.html', {'img_name': img_name})
+            return redirect(to='login')
+
+        return render(request, self.template_name, {'form': form})
+
+
+# Class based view that extends from the built in login view to add a remember me functionality
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+
+    # def form_valid(self, form):
+    #     remember_me = form.cleaned_data.get('remember_me')
+
+    #     if not remember_me:
+    #         # set session expiry to 0 seconds. So it will automatically close the session after the browser is closed.
+    #         self.request.session.set_expiry(0)
+
+    #         # Set session as modified to force data updates/cookie to be saved.
+    #         self.request.session.modified = True
+
+    #     # else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
+    #     return super(CustomLoginView, self).form_valid(form)
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    subject_template_name = 'users/password_reset_subject'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('users-home')
+
+
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'users/change_password.html'
+    success_message = "Successfully Changed Your Password"
+    success_url = reverse_lazy('users-home')
+
+
+
+
+def banklogin(request):
+    if request.method == 'POST1':
+        print("hii")
+    return render(request, 'users/profile.html')
+
+@login_required()
+class profile(View):
+    form_class = RegisterForm
+    initial = {'key': 'value'}
+    template_name = 'users/register.html'
+
+
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+
+            username = form.cleaned_data.get('username')
+            adhaar = form.cleaned_data.get('adhaar_no')
+            pan = form.cleaned_data.get('pan_no')
+            address = form.cleaned_data.get('last_name')
+            firstname = form.cleaned_data.get('first_name')
+            full ="Name" + "\n" + str(firstname) + "\n"+ "Address" + "\n" + str(address) + "\n"+ "Adhar number" + "\n" + str(adhaar) + "\n" +"Pan number" + "\n" + str(pan)
+            img = qrcode.make(full)
+            img_name = 'qr' + str(time.time()) + '.png'
+            img.save(settings.MEDIA_ROOT + '/' + img_name)
+            messages.success(request, f'Account created for {username} {adhaar}')
+            return render(request, 'users/index.html', {'img_name': img_name})
+            return redirect(to='login')
+
+        return render(request, self.template_name, {'form': form})
